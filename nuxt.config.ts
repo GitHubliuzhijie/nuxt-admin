5bb79e5a4b44d3dfdbd6448df3931d31d6dca679
liuzhijie <l2002_0916@163.com>
2024-03-12 10:36:11 +0800

    :sparkles:  项目初始化

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+export default defineNuxtConfig({
+  devServer: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  ssr: false,
+  app: {
+    keepalive: true,
+  },
+  experimental: {
+    payloadExtraction: false,
+  },
+  sourcemap: {
+    server: true,
+    client: false,
+  },
+  css: [
+    "element-plus/dist/index.css",
+    "element-plus/theme-chalk/dark/css-vars.css",
+    "@/assets/css/main.css",
+  ],
+  devtools: { enabled: true },
+});
---
+++ pages/admin/Welcome.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>111111111111</div>
+</template>
